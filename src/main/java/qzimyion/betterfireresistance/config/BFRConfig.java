047db79fb5ae89dde769f2578b1da238f8b5cef0
Qzimyion <samyakchauhan676@gmail.com>
2024-06-14 17:08:11 +0530

    Updated the mod to 1.20.6 with a multiloader format

--- src/main/java/qzimyion/betterfireresistance/config/BFRConfig.java
+++
@@ -1,9 +0,0 @@
-package qzimyion.betterfireresistance.config;
-
-import eu.midnightdust.lib.config.MidnightConfig;
-
-public class BFRConfig extends MidnightConfig {
-
-    @Entry(category = "rendering")
-    public static boolean defaultFireOffInCreavite = false;
-}
