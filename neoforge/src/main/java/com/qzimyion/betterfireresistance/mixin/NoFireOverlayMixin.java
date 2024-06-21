e9e5babc3e984dccac3cb00795b74697ed1a5e4c
Qzimyion <samyakchauhan676@gmail.com>
2024-06-21 17:04:30 +0530

    Fixed Fabric side mixin

--- neoforge/src/main/java/com/qzimyion/betterfireresistance/mixin/NoFireOverlayMixin.java
+++ neoforge/src/main/java/com/qzimyion/betterfireresistance/mixin/NoFireOverlayMixin.java
@@ -5,9 +5,12 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ScreenEffectRenderer;
 import net.minecraft.world.effect.MobEffects;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+@OnlyIn(Dist.CLIENT)
 @Mixin(ScreenEffectRenderer.class)
 public class NoFireOverlayMixin {
 
